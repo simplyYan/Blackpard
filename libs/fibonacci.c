69b0e175fb2c639c1885a2776342a359d693a0ef
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-01-10 12:42:49 +0000

    Update fibonacci.c

--- libs/fibonacci.c
+++ libs/fibonacci.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 
 #define MAX 100
 #define NIL -1
 
 long long memo[MAX];
 
+// Initializes the memo array with NIL
 void initialize() {
     int i;
     for (i = 0; i < MAX; i++) {
@@ -12,6 +14,7 @@ void initialize() {
     }
 }
 
+// Efficiently calculates the Fibonacci number using memoization
 long long fibonacci(int n) {
     if (memo[n] == NIL) {
         if (n <= 1) {
@@ -26,9 +29,19 @@ long long fibonacci(int n) {
 int main() {
     initialize();
 
-    int n = 55;
+    int n = 55; // Calculate the 55th term of the Fibonacci sequence
 
-    printf("The %th term of the Fibonacci sequence is: %lld\n", n, fibonacci(n));
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    long long result = fibonacci(n);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("The %dth term of the Fibonacci sequence is: %lld\n", n, result);
+    printf("Execution time: %f seconds\n", cpu_time_used);
 
     return 0;
 }
