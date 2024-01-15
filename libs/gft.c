93922c5b4e994bf8e0dcb336e41b79b2344253eb
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-01-15 14:10:21 +0000

    Add files via upload

---
+++ libs/gft.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void echoS(const char *str) {
+    printf("%s\n", str);
+}
+
+void echoI(int num) {
+    printf("%d\n", num);
+}
+
+void echoF(float num) {
+    printf("%f\n", num);
+}
