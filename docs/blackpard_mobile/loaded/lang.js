1fa4c6bef733c6f29c7be1df744bf5a271a38763
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-03-09 18:39:11 +0000

    Update lang.js

--- docs/blackpard_mobile/loaded/lang.js
+++ docs/blackpard_mobile/loaded/lang.js
@@ -258,21 +258,126 @@ function maybe(conditions) {
     }
 };
 
-const Pardrunner = (func) => {
-  const cache = new Map();
+function memoize(fn, options) {
+  var cache = options && options.cache
+    ? options.cache
+    : cacheDefault;
 
-  return (...args) => {
-    const key = JSON.stringify(args);
+  var serializer = options && options.serializer
+    ? options.serializer
+    : serializerDefault;
 
-    if (cache.has(key)) {
-      console.log('Returning the memoized value:', cache.get(key));
-      return cache.get(key);
-    } else {
-      const result = func(...args);
-      cache.set(key, result);
-      console.log('Calculating and memoizing new value:', result);
-      return result;
-    }
-  };
+  var strategy = options && options.strategy
+    ? options.strategy
+    : strategyDefault;
+
+  return strategy(fn, {
+    cache: cache,
+    serializer: serializer
+  });
+}
+
+function isPrimitive(value) {
+  return value == null || typeof value === 'number' || typeof value === 'boolean';
+}
+
+function monadic(fn, cache, serializer, arg) {
+  var cacheKey = isPrimitive(arg) ? arg : serializer(arg);
+
+  var computedValue = cache.get(cacheKey);
+  if (typeof computedValue === 'undefined') {
+    computedValue = fn.call(this, arg);
+    cache.set(cacheKey, computedValue);
+  }
+
+  return computedValue;
+}
+
+function variadic(fn, cache, serializer) {
+  var args = Array.prototype.slice.call(arguments, 3);
+  var cacheKey = serializer(args);
+
+  var computedValue = cache.get(cacheKey);
+  if (typeof computedValue === 'undefined') {
+    computedValue = fn.apply(this, args);
+    cache.set(cacheKey, computedValue);
+  }
+
+  return computedValue;
+}
+
+function assemble(fn, context, strategy, cache, serialize) {
+  return strategy.bind(
+    context,
+    fn,
+    cache,
+    serialize
+  );
+}
+
+function strategyDefault(fn, options) {
+  var strategy = fn.length === 1 ? monadic : variadic;
+
+  return assemble(
+    fn,
+    this,
+    strategy,
+    options.cache.create(),
+    options.serializer
+  );
+}
+
+function strategyVariadic(fn, options) {
+  var strategy = variadic;
+
+  return assemble(
+    fn,
+    this,
+    strategy,
+    options.cache.create(),
+    options.serializer
+  );
+}
+
+function strategyMonadic(fn, options) {
+  var strategy = monadic;
+
+  return assemble(
+    fn,
+    this,
+    strategy,
+    options.cache.create(),
+    options.serializer
+  );
+}
+
+function serializerDefault() {
+  return JSON.stringify(arguments);
+}
+
+function ObjectWithoutPrototypeCache() {
+  this.cache = Object.create(null);
+}
+
+ObjectWithoutPrototypeCache.prototype.has = function(key) {
+  return (key in this.cache);
 };
 
+ObjectWithoutPrototypeCache.prototype.get = function(key) {
+  return this.cache[key];
+};
+
+ObjectWithoutPrototypeCache.prototype.set = function(key, value) {
+  this.cache[key] = value;
+};
+
+var cacheDefault = {
+  create: function create() {
+    return new ObjectWithoutPrototypeCache();
+  }
+};
+
+var strategies = {
+  variadic: strategyVariadic,
+  monadic: strategyMonadic
+};
